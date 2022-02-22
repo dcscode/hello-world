7546d52ee087cc50e7102d11b8ace3a4929f26dc
Delaney Cifranik-Stovall <delaneycscode@gmail.com>
2022-02-22 15:52:35 +0000

    initial, part of example commit

---
+++ javascript/stateCodeAlong/main.js
@@ -0,0 +1,17 @@
+//INIT
+// create the state object
+
+let scoreBoard = new ScoreBoard();
+
+function init() {
+    console.log("hello world");
+}
+
+class ScoreBoard {
+
+    static constructor() {
+        let data = []; //scoreBoard.data could be something like JSON data
+    }
+    //properties
+
+}
