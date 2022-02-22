8504877a5c64123f5c5304b75469dd3b206eae13
Delaney Cifranik-Stovall <delaneycscode@gmail.com>
2022-02-22 16:13:04 +0000

    construct a scoreboard that can add balls, strikes, and outs

--- javascript/stateCodeAlong/main.js
+++ javascript/stateCodeAlong/main.js
@@ -1,7 +1,4 @@
 //INIT
-// create the state object
-
-let scoreBoard = new ScoreBoard();
 
 function init() {
     console.log("hello world");
@@ -9,14 +6,32 @@ function init() {
 
 class ScoreBoard {
 
-    static constructor() {
+    constructor() {
         //properties
-        let data = []; //scoreBoard.data could be something like JSON data
-        let balls = [];
-        let strikes = 0;
-        let outs = 0;
-        let innings = [];
+        this.fieldName = "Wrigley Field";
+        this.data = []; //scoreBoard.data could be something like JSON data
+        this.balls = 0;
+        this.strikes = 0;
+        this.outs = 0;
+        this.innings = [];
+    }
+
+    addBall () {
+        //do one thing
+        //gets info from data
+        this.balls++;
+    }
+
+    addStrike () {
+        this.strikes++;
+    }
+
+    addOut () {
+        this.outs++;
     }
     
 
 }
+
+// create the state object
+let scoreBoard = new ScoreBoard();
