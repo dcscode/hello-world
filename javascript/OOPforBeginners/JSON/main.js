41e1bb206a7537139fa95568e6ecfdfd01b375c0
Delaney Cifranik-Stovall <delaney.cifranik-stovall@student.awesomeinc.org>
2022-02-23 20:41:35 +0000

    rename JSON.js file, begin main.js, add function to fetch from API,add function to create content that displays data from JSON object

--- javascript/OOPforBeginners/JSON/main.js
+++ javascript/OOPforBeginners/JSON/main.js
@@ -1,42 +1,21 @@
-//JSON object
-    const superHeroes = {
-      "squadName" : "Super hero squad",
-      "homeTown" : "Metro City",
-      "formed" : 2016,
-      "secretBase" : "Super tower",
-      "active" : true,
-      "members" : [
-        {
-          "name" : "Molecule Man",
-          "age" : 29,
-          "secretIdentity" : "Dan Jukes",
-          "powers" : [
-            "Radiation resistance",
-            "Turning tiny",
-            "Radiation blast"
-          ]
-        },
-        {
-          "name" : "Madame Uppercut",
-          "age" : 39,
-          "secretIdentity" : "Jane Wilson",
-          "powers" : [
-            "Million tonne punch",
-            "Damage resistance",
-            "Superhuman reflexes"
-          ]
-        },
-        {
-          "name" : "Eternal Flame",
-          "age" : 1000000,
-          "secretIdentity" : "Unknown",
-          "powers" : [
-            "Immortality",
-            "Heat Immunity",
-            "Inferno",
-            "Teleportation",
-            "Interdimensional travel"
-          ]
-        }
-      ]
-    }
+async function populate() {
+    const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    const superHeroes = await response.json();
+
+    populateHeader(superHeroes);
+    populateHeroes(superHeroes);
+}
+
+function populateHeader(obj) {
+    const header = document.querySelector('header');
+    const myH1 = document.createElement('h1');
+    myH1.textContent = obj['squadName'];
+    header.appendChild(myH1);
+
+    const myPara = document.createElement('p');
+    myPara.textContent = `Hometown: ${obj['homeTown']} // Formed: ${obj['formed']}`;
+    header.appendChild(myPara);
+}
