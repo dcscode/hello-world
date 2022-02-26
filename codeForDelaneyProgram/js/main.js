1f6d0e2591cdc6be3a7b901c0408c435b2273803
Delaney Cifranik-Stovall <delaney.cifranik-stovall@student.awesomeinc.org>
2022-02-26 14:21:08 +0000

    my pseudocode

---
+++ codeForDelaneyProgram/js/main.js
@@ -0,0 +1,90 @@
+//I added neurodivergence to this workspace because my spell extension didn't work
+//for purposes of this demo, neurodivergence is only a recognized "word" here in this workspace, my playground
+//in reality, neurodivergence should be scoped to user instead (inside the spellcheck extension)
+const neurodivergence = document.querySelector("Delaney"); 
+
+/*
+let neurodivergence = {
+    [
+        {
+            neurodivergent: [
+
+            ]
+        },
+        {
+            neurotypical: [
+
+            ]
+        }
+
+        ]
+    }
+    */
+
+
+    //my whole life
+    //my mom was in the neurodivergent scope and I couldn't access it from within the scope of the spectrum
+    //I didn't know my place in the scope
+    //I was inside spectrum inside Autism but I couldn't access neurodivergent
+    //I didn't know I was in a spectrum
+    //I thought neurodivergent === spectrum === autism
+    //I iterated up correctly to my mom using the AwesomeInc "Trust the Process" method
+    //my mom was in the global scope she could see everything but I couldn't
+    //her function helpDelaney didn't run right; it's methods didn't return anything Delaney inside spectrum inside autism could understand
+    //she had to update the data her function was calling with better data
+    //and now we can communicate
+    //im 25 and now i understand my scope
+    //i was stuck inside autism i didn't know I was inside a spectrum
+    //my mom had work data in mom function
+    //but mom function didn't understand how far I was
+    //we each had to update our functions so we could talk to each each other
+    //I understand outside my scope but I didn't know I was inside somewhere my mom couldn't hear
+    //when I was initiated i was already inside my autism scope, i didn't know anyone above me
+    //so far, learning this model of the neurodivergence concept has helped me communicate with my mom
+    //inside my scope i had figured out to drop mask
+    //so when I ran my code everything within my scope worked (people I got to know/met after I dropped my mask)
+    //i thought my mom was in neurotypical so my code didn't work, that made sense
+    //my mom is above me. im the child in her parent scope
+    //and now all her methods (her trying to help me) make sense, i can access them now
+    //her methods couldn't reach me at first (until we both knew our place in the scope)
+    //now we know which scope we're in and our code will finally work
+    //luckily, my mom so happens to have access to the updated relevant data and can funnel it to me
+    //not everyone has my mom, not everyone's parent scope has access to that variable
+    //now that we're in scope together and know where we go and where each other
+    //she can use her personal parent methods
+    //I want to make sure no one is ever lost in scope like that
+    //I want everyone to be able to access that global data but from where they are, lost in the scope
+    //Im 25 and I finally can be understood the way I want where I want
+    //I never want anyone to go through being so close but so far
+    //by sheer chance and accident I am still alive
+    //ive spent so long feeling like I was broken and that since i couldn't fix myself, i should nuke all my code
+    //ive been so close for two weeks and my mom had to take that risk to find me in my scope
+    //she was afraid i didn't even know I was in the neurodivergent object
+    //her methods made my code make sense
+    //i want to be able to help people in their scope find where they are and then they can navigate all of them
+    //im the me im supposed to be but i couldn't access those properties and my mom didn't know that her methods didn't run
+    //i thought i wanted a job that made it easier to be me; punch in punch out people don't hurt me
+    //i want to help people understand they're not broken, there's nothing missing
+    //they need someone to help them find their scope
+
+
+
+
+
+
+
+
+    //from the object neurodivergence 
+        //scope into an array of objects
+            //the properties of these objects are
+                //neurodivergent
+                    //if (spectrum === true) //if the index of neurodivergent[definition] === spectrum
+                        //access the properties within spectrum object
+                            //spectrum.properties[typesOfNeurodivergence]
+                                //ADHD
+                                //Autism (I'm here);
+                                // . .
+
+                //neurotypical
+                    //spectrum != true ----> not sure
+                        //people I haven't met
